ba5edfd016da6d12a97409b59c73af453602b571
abeltpontus-art <abeltpontus@gmail.com>
2026-02-08 02:41:16 +0700

    Create next.config.js

---
+++ src/styles/next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true
+};
+module.exports = nextConfig;
+
+-------------------------
+FILE: postcss.config.js
+-------------------------
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {}
+  }
+};
