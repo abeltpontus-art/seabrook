317baff7690590287f399a2e78c4c24ba8950e94
abeltpontus-art <abeltpontus@gmail.com>
2026-02-08 03:10:25 +0700

    Create page.tsx

---
+++ src/app/properties/page.tsx
@@ -0,0 +1,8 @@
+export default function PropertiesPage() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>Available Properties</h1>
+      <p>Listings coming soon.</p>
+    </main>
+  );
+}
